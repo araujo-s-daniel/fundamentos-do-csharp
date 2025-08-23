5afcca1c6e60d0d79c9391a4ee12fe8c54e04956
Daniel Araújo <daniels_araujo@outlook.com.br>
2025-08-23 11:30:08 -0300

    04.07 - Trabalhando com variáveis

--- Curso/ProjetoAulas/Program.cs
+++ Curso/ProjetoAulas/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Primeiro Programa");
+
+int idade = 33;
+Console.WriteLine(idade);
+string nomePessoa = "Rafael Almeida";
+Console.WriteLine(nomePessoa);
+decimal valor = 200.99m;
+double valorDouble = 200.99;
+float valorFloat = 200.99f;
+Console.WriteLine(valor);
+Console.WriteLine(valorDouble);
+Console.WriteLine(valorFloat);
+var idadeNova = 34;
+char flag = 'Y';
+Console.WriteLine(flag);
+bool ativo = true;
+Console.WriteLine(ativo);
