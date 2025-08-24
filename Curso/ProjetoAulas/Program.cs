e7b503e77b9f9b88157c6e5dfc2f670ed2e0d3ec
Daniel Araújo <daniels_araujo@outlook.com.br>
2025-08-24 08:10:18 -0300

    07.02 - IF / ELSE IF / ELSE

--- Curso/ProjetoAulas/Program.cs
+++ Curso/ProjetoAulas/Program.cs
@@ -207,7 +207,6 @@ Console.WriteLine(nome2);
 // {
 //     Console.WriteLine(item);
 // }
-*/
 
 var stack = new Stack<string>();
 
@@ -223,3 +222,54 @@ Console.WriteLine(nome2);
 // {
 //     Console.WriteLine(item);
 // }
+*/
+
+// var diaDaSemana = 0;
+// var diaDeTrabalho = false;
+
+// if (diaDaSemana == 0 && diaDeTrabalho == true)
+// {
+//     Console.WriteLine("Hoje é domingo");
+// }
+// else if (diaDaSemana == 0 && diaDeTrabalho  == false) {
+//     Console.WriteLine("Hoje é domingo, mas não é dia de trabalho");
+// }
+// else
+// {
+//     Console.WriteLine("Hoje não é domingo");
+// }
+
+var diaDaSemana = 7;
+
+if (diaDaSemana == 0)
+{
+    Console.WriteLine("Hoje é domingo");
+}
+else if (diaDaSemana == 1)
+{
+    Console.WriteLine("Hoje é segunda");
+}
+else if (diaDaSemana == 2)
+{
+    Console.WriteLine("Hoje é terça");
+}
+else if (diaDaSemana == 3)
+{
+    Console.WriteLine("Hoje é quarta");
+}
+else if (diaDaSemana == 4)
+{
+    Console.WriteLine("Hoje é quinta");
+}
+else if (diaDaSemana == 5)
+{
+    Console.WriteLine("Hoje é sexta");
+}
+else if (diaDaSemana == 6)
+{
+    Console.WriteLine("Hoje é sábado");
+}
+else
+{
+    Console.WriteLine("Dia da semana inválido");
+}
