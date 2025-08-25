2eecc221937814bee4193cef56e2651fcaecc536
Daniel Araújo <daniels_araujo@outlook.com.br>
2025-08-25 18:26:05 -0300

    15.02 - Criando projeto AppClientes

---
+++ Curso/AppClientes/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace AppClientes;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Menu();
+            Console.ReadKey();
+        }
+    }
+
+    static void Menu()
+    {
+        Console.Clear();
+        Console.WriteLine("Cadastro de Clientes");
+        Console.WriteLine("--------------------");
+        Console.WriteLine("1 - Cadastrar Cliente");
+        Console.WriteLine("2 - Exibir Clientes");
+        Console.WriteLine("3 - Editar Cliente");
+        Console.WriteLine("4 - Excluir Cliente");
+        Console.WriteLine("5 - Sair");
+        Console.WriteLine("--------------------");
+    }
+}
