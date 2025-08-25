e8f54201c62654c22a7c7e14397af50a7a23107b
Daniel Araújo <daniels_araujo@outlook.com.br>
2025-08-25 07:13:57 -0300

    08.04 - O que é uma classe

--- Curso/ProjetoAulas/Program.cs
+++ Curso/ProjetoAulas/Program.cs
@@ -8,7 +8,19 @@ namespace Application
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            AulaClasses();
+        }
+
+        private static void AulaClasses()
+        {
+            // var produto = new Cadastro.Produto();
+            // produto.SetId(1);
+            // produto.Descricao = "Teclado";
+            // produto.ImprimirDescricao();
+            // Console.WriteLine(produto.GetId());
+
+            var resultado = Cadastro.Calculos.SomarNumeros(2, 3);
+            Console.WriteLine(resultado);
         }
     }
 }
